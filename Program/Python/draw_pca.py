10cfcbaeb326ace8b31903b1384afaf2d0d5a2be
Jaewoong Lee <230@fumire.moe>
2026-09-18 10:55:29 +0900

    Add PCA key
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- Program/Python/draw_pca.py
+++ Program/Python/draw_pca.py
@@ -47,8 +47,8 @@ if __name__ == "__main__":
 
             seaborn.scatterplot(data=pca_data.sort_values(gene, ascending=True), x=step00.projection_columns[0], y=step00.projection_columns[1], hue=gene, palette="Reds", legend="brief", rasterized=True, s=40, edgecolor=None, ax=ax)
 
-            matplotlib.pyplot.xlabel(f"PC-1 ({input_adata.uns['pca']['variance_ratio'][0] * 100:.1f}%)")
-            matplotlib.pyplot.ylabel(f"PC-2 ({input_adata.uns['pca']['variance_ratio'][1] * 100:.1f}%)")
+            matplotlib.pyplot.xlabel(f"PC-1 ({input_adata.uns[step00.pca_key]['variance_ratio'][0] * 100:.1f}%)")
+            matplotlib.pyplot.ylabel(f"PC-2 ({input_adata.uns[step00.pca_key]['variance_ratio'][1] * 100:.1f}%)")
             matplotlib.pyplot.title(gene)
             ax.set_xticklabels([])
             ax.set_yticklabels([])
@@ -61,12 +61,12 @@ if __name__ == "__main__":
             matplotlib.pyplot.close(fig)
 
         fig, ax = matplotlib.pyplot.subplots(figsize=(24, 18))
-        for i, variance in tqdm.contrib.tenumerate(input_adata.uns["pca"]["variance_ratio"], start=1):
+        for i, variance in tqdm.contrib.tenumerate(input_adata.uns[step00.pca_key]["variance_ratio"], start=1):
             i = i[0] + 1
             matplotlib.pyplot.text(i, variance, f"PC{i}", color="black", fontsize="x-small", horizontalalignment="center", rotation="vertical", verticalalignment="bottom", path_effects=step00.path_effects)
 
-        matplotlib.pyplot.xticks(range(len(input_adata.uns["pca"]["variance_ratio"])), ["" for _ in input_adata.uns["pca"]["variance_ratio"]])
-        matplotlib.pyplot.ylim(bottom=0, top=1.25 * max(input_adata.uns["pca"]["variance_ratio"]))
+        matplotlib.pyplot.xticks(range(len(input_adata.uns[step00.pca_key]["variance_ratio"])), ["" for _ in input_adata.uns[step00.pca_key]["variance_ratio"]])
+        matplotlib.pyplot.ylim(bottom=0, top=1.25 * max(input_adata.uns[step00.pca_key]["variance_ratio"]))
         matplotlib.pyplot.xlabel("Ranking")
         matplotlib.pyplot.ylabel("Explained variance")
         matplotlib.pyplot.grid(True)
