cc37bf6ead0e8556405e8c89818b43fb700e22a3
Jaewoong Lee <230@fumire.moe>
2026-09-20 16:15:24 +0900

    Add figures
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- Program/Python/draw_xenium_coordinates.py
+++ Program/Python/draw_xenium_coordinates.py
@@ -8,6 +8,7 @@ import matplotlib
 import matplotlib.colors
 import matplotlib.pyplot
 import numpy
+import pandas
 import scanpy
 import seaborn
 import tqdm
@@ -45,37 +46,17 @@ if __name__ == "__main__":
     with tempfile.TemporaryDirectory() as directory:
         figure_list: typing.List[str] = list()
 
-        fig, ax = matplotlib.pyplot.subplots(figsize=(18, 18))
-
-        seaborn.scatterplot(data=coordinate_data, x=step00.spatial_columns[0], y=step00.spatial_columns[1], hue=step00.celltype_column, hue_order=cell_type_list, palette=cell_type_palette, style=step00.sample_column, style_order=sample_list, rasterized=True, s=30, edgecolor=None, ax=ax)
-
-        for sample in tqdm.tqdm(sample_list):
-            step00.confidence_ellipse(coordinate_data.loc[(coordinate_data[step00.sample_column] == sample), step00.spatial_columns[0]], coordinate_data.loc[(coordinate_data[step00.sample_column] == sample), step00.spatial_columns[1]], ax=ax, edgecolor="black", linewidth=2.5)
-            matplotlib.pyplot.text(numpy.mean(coordinate_data.loc[(coordinate_data[step00.sample_column] == sample), step00.spatial_columns[0]]), numpy.mean(coordinate_data.loc[(coordinate_data[step00.sample_column] == sample), step00.spatial_columns[0]]), sample, horizontalalignment="center", verticalalignment="center", fontsize="small", color="black", path_effects=step00.path_effects)
-
-        matplotlib.pyplot.xlabel(f"{step00.spatial_columns[0]} (μm)")
-        matplotlib.pyplot.ylabel(f"{step00.spatial_columns[1]} (μm)")
-        matplotlib.pyplot.title(f"Total (Cell n={len(coordinate_data)})")
-        matplotlib.pyplot.legend(loc="upper right")
-        matplotlib.pyplot.tight_layout()
-
-        figure_list.append(f"{directory}/Scatter-{step00.celltype_column}.pdf")
-        fig.savefig(figure_list[-1])
-        figure_list.append(f"{directory}/Scatter-{step00.celltype_column}.png")
-        fig.savefig(figure_list[-1])
-        matplotlib.pyplot.close(fig)
-
         for sample in tqdm.tqdm(sample_list):
             drawing_data = coordinate_data.loc[(coordinate_data[step00.sample_column] == sample)]
+            minimum = numpy.min(drawing_data[[*step00.spatial_columns]].to_numpy()) - 500
+            maximum = numpy.max(drawing_data[[*step00.spatial_columns]].to_numpy()) + 500
 
             fig, ax = matplotlib.pyplot.subplots(figsize=(18, 18))
 
-            seaborn.scatterplot(data=drawing_data, x=step00.spatial_columns[0], y=step00.spatial_columns[1], hue=step00.celltype_column, hue_order=cell_type_list, palette=cell_type_palette, rasterized=True, s=30, edgecolor=None, ax=ax)
+            seaborn.scatterplot(data=drawing_data, x=step00.spatial_columns[0], y=step00.spatial_columns[1], hue=step00.celltype_column, hue_order=cell_type_list, palette=cell_type_palette, rasterized=True, s=15, edgecolor=None, ax=ax)
 
-            for cell_type in tqdm.tqdm(cell_type_list):
-                step00.confidence_ellipse(drawing_data.loc[(drawing_data[step00.celltype_column] == cell_type), step00.spatial_columns[0]], drawing_data.loc[(drawing_data[step00.celltype_column] == cell_type), step00.spatial_columns[1]], ax=ax, edgecolor="black", linewidth=2.5)
-                matplotlib.pyplot.text(numpy.mean(drawing_data.loc[(drawing_data[step00.celltype_column] == cell_type), step00.spatial_columns[0]]), numpy.mean(drawing_data.loc[(drawing_data[step00.celltype_column] == cell_type), step00.spatial_columns[1]]), cell_type, horizontalalignment="center", verticalalignment="center", fontsize="small", color="black", path_effects=step00.path_effects)
-
+            matplotlib.pyplot.xlim(minimum, maximum)
+            matplotlib.pyplot.ylim(minimum, maximum)
             matplotlib.pyplot.xlabel(f"{step00.spatial_columns[0]} (μm)")
             matplotlib.pyplot.ylabel(f"{step00.spatial_columns[1]} (μm)")
             matplotlib.pyplot.title(f"{sample} (Cell n={len(drawing_data)})")
@@ -87,6 +68,28 @@ if __name__ == "__main__":
             figure_list.append(f"{directory}/Scatter-{sample}.png")
             fig.savefig(figure_list[-1])
             matplotlib.pyplot.close(fig)
+
+        for sample in tqdm.tqdm(sample_list):
+            drawing_data = coordinate_data.loc[(coordinate_data[step00.sample_column] == sample)]
+            minimum = numpy.min(drawing_data[[*step00.spatial_columns]].to_numpy()) - 500
+            maximum = numpy.max(drawing_data[[*step00.spatial_columns]].to_numpy()) + 500
+            highlight_data = pandas.concat({cell_type: drawing_data.assign(Highlight=numpy.where(drawing_data[step00.celltype_column] == cell_type, cell_type, step00.rest_value)) for cell_type in cell_type_list}, names=["Panel"]).reset_index(level="Panel")
+
+            g = seaborn.relplot(data=highlight_data, x=step00.spatial_columns[0], y=step00.spatial_columns[1], hue="Highlight", hue_order=[step00.rest_value] + cell_type_list, palette=cell_type_palette | {step00.rest_value: step00.rest_color}, col="Panel", col_wrap=4, kind="scatter", s=10, linewidth=0, height=18, aspect=1, legend=False, facet_kws={"sharex": True, "sharey": True})
+            for ax in g.axes.flat:
+                ax.set_aspect("equal")
+                ax.invert_yaxis()
+                ax.set_xticks([])
+                ax.set_yticks([])
+                ax.set_xlabel("")
+                ax.set_ylabel("")
+            g.set_titles("{col_name}",)
+
+            figure_list.append(f"{directory}/Relplot-{sample}.pdf")
+            g.savefig(figure_list[-1])
+            figure_list.append(f"{directory}/Relplot-{sample}.png")
+            g.savefig(figure_list[-1])
+            matplotlib.pyplot.close(g.fig)
 
         with zipfile.ZipFile(args.output, mode="w", compression=zipfile.ZIP_DEFLATED, compresslevel=9) as zip_file:
             for figure in tqdm.tqdm(figure_list):
