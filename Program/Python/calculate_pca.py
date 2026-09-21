777502149060ab1fc27741016768ca8f3b5b418a
Jaewoong Lee <230@fumire.moe>
2026-09-21 10:36:40 +0900

    Do not select HVG
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- Program/Python/calculate_pca.py
+++ Program/Python/calculate_pca.py
@@ -17,12 +17,12 @@ if __name__ == "__main__":
     input_adata = scanpy.read_h5ad(args.input)
     print(input_adata)
 
-    rapids_singlecell.pp.pca(input_adata, layer=step00.log_column, svd_solver="covariance_eigh", random_state=42, key_added=step00.pca_key)
+    rapids_singlecell.pp.pca(input_adata, layer=step00.log_column, mask_var=None, svd_solver="covariance_eigh", random_state=42, key_added=step00.pca_key)
     print(input_adata)
 
     rapids_singlecell.pp.harmony_integrate(input_adata, key=step00.sample_column, basis=step00.pca_key, adjusted_basis=step00.harmony_key, correction_method="batched", random_state=42)
     print(input_adata)
 
-    rapids_singlecell.pp.neighbors(input_adata, n_pcs=10, algorithm="ivfpq", random_state=42)
+    rapids_singlecell.pp.neighbors(input_adata, use_rep=step00.harmony_key, algorithm="ivfpq", random_state=42)
     print(input_adata)
     input_adata.write_h5ad(args.output, **step00.anndata_compressions)
