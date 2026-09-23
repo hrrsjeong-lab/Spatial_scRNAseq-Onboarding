a34773de90a44ba13a659f61d76efc5d0436c01a
Jaewoong Lee <230@fumire.moe>
2026-09-23 15:48:30 +0900

    Bug fixed
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- Program/Python/calculate_phate.py
+++ Program/Python/calculate_phate.py
@@ -25,7 +25,6 @@ if __name__ == "__main__":
     projection_input = numpy.ascontiguousarray(input_adata.obsm[step00.harmony_key], dtype=numpy.float32)
     print("PHATE input:", projection_input.shape, projection_input.dtype)
 
-    phate_operator = phate.PHATE(n_components=2, knn=5, decay=15, n_landmark=args.landmark, t="auto", gamma=1.0, n_pca=None, knn_dist="euclidean", mds_dist="euclidean", mds="metric", n_jobs=args.cpus, random_state=42, verbose=1)
-    input_adata.obsm[step00.projection_key] = phate_operator.fit_transform(projection_input)
+    input_adata.obsm[step00.projection_key] = phate.PHATE(n_components=2, knn=5, decay=15, t="auto", gamma=1.0, n_pca=None, knn_dist="euclidean", mds_dist="euclidean", mds="metric", n_jobs=args.cpus, random_state=42, verbose=True).fit_transform(projection_input)
     print(input_adata)
     input_adata.write_h5ad(args.output, **step00.anndata_compressions)
